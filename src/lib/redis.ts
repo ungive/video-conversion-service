0e71edf1b39045a78e38d4e0ac9600ac00266a1a
Jonas van den Berg <jonas@vandenbe.com>
2025-07-22 21:00:39 +0200

    Add debugging for adding jobsSub listeners

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
+import { AsyncCounter } from './async-counter'
 
 export const REDIS_KEYSPACE_PREFIX = '__keyspace@0__'
+
+const debugSubCounter = new AsyncCounter()
 
 export function waitForKey(
   server: FastifyInstance,
@@ -20,6 +23,11 @@ export function waitForKey(
         finished = true
         clearTimeout(timer)
         server.jobsSub.removeListener('message', onMessage)
+        if (server.config.debug) {
+          debugSubCounter.decrement().then(count => {
+            server.log.debug({ count }, 'removed jobsSub listener')
+          })
+        }
         signal?.removeEventListener('abort', onAbort)
       }
     }
@@ -49,6 +57,11 @@ export function waitForKey(
     }
 
     server.jobsSub.on('message', onMessage)
+    if (server.config.debug) {
+      debugSubCounter.increment().then(count => {
+        server.log.debug({ count }, 'added jobsSub listener')
+      })
+    }
 
     if (timeout !== undefined) {
       timer = setTimeout(() => {
